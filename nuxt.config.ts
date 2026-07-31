489ec7c24ea6bfb66095f457b3fd59b651417e50
gregandcin <gregandcinbusiness@gmail.com>
2026-07-31 16:46:19 -0400

    fix: don't reset game settings on Reset Game, update deps (#12)
    * chore: bump NPM and Node.js versions
    
    * chore: update node deps
    
    * chore: update optimizeDeps
    
    * fix: don't reset configs when clicking Reset Game button

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
-      include: ["@vue/devtools-core", "@vue/devtools-kit"],
+      include: ["@vue/devtools-core", "@vue/devtools-kit", "@lucide/vue"],
     },
   },
   pwa: {
